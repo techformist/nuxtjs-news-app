b8bf6685c0f6bae5d66f298e58052f41e72a9618
Prashanth Krishnamurthy <prashanth1krishnamurthy@gmail.com>
2019-01-08 12:21:24 +0530

    v1 Initial version

---
+++ layouts/default.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer :mini-variant="miniVariant" :clipped="clipped" v-model="drawer" fixed app>
+      <v-list>
+        <v-list-tile v-for="(item, i) in items" :to="item.to" :key="i" router exact>
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"/>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"/>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar :clipped-left="clipped" fixed app>
+      <v-toolbar-side-icon @click="drawer = !drawer"/>
+
+      <v-toolbar-title v-text="title"/>
+    </v-toolbar>
+    <v-content>
+      <v-container>
+        <nuxt/>
+      </v-container>
+    </v-content>
+    <v-navigation-drawer :right="right" v-model="rightDrawer" temporary fixed>
+      <v-list>
+        <v-list-tile @click.native="right = !right">
+          <v-list-tile-action>
+            <v-icon light>compare_arrows</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: true,
+      drawer: true,
+      fixed: false,
+      items: [
+        { icon: "apps", title: "Welcome", to: "/" },
+        { icon: "bubble_chart", title: "Inspire", to: "/inspire" },
+        { icon: "language", title: "News", to: "/news" },
+        { icon: "group_work", title: "About", to: "/about" }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: "Awesome News App"
+    };
+  }
+};
+</script>
