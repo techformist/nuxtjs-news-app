97318c369fa855e4b4d6217bc24bef6e9503b452
Prashanth Krishnamurthy <prashanth1krishnamurthy@gmail.com>
2019-01-08 12:21:24 +0530

    v1 Initial version

---
+++ pages/about.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <v-container>
+      <h2>About</h2>
+
+      <v-layout>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime voluptatibus ut beatae quidem repudiandae exercitationem molestiae nisi commodi non alias, enim odio iusto distinctio neque accusamus voluptatem facere fugit quos harum voluptas fugiat dolorem! Recusandae alias, vitae consequuntur temporibus minima suscipit atque inventore odit culpa aspernatur quia sequi, porro earum? Voluptas eaque veritatis velit nostrum! Pariatur illo nobis saepe tempora eius magni non totam dignissimos repudiandae, reprehenderit laborum odio soluta eos quo commodi aliquid blanditiis dolor eveniet quaerat numquam itaque placeat et! Temporibus fuga, aperiam, id, similique rem a voluptatum minus perferendis consectetur voluptatem iusto neque cupiditate mollitia officiis dignissimos!</v-layout>
+    </v-container>
+  </div>
+</template>
---
+++ pages/news.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <v-container>
+      <v-flex xs12 pb-5>
+        <h2>News</h2>
+      </v-flex>
+
+      <v-flex>
+        <!-- News !-->
+        <v-flex xs12 pb-3 sm6 offset-sm3 v-for="(article, index) in articles" :key="index">
+          <v-card>
+            <img :src="article.urlToImage" width="100%" height="200px">
+
+            <v-card-title primary-title>
+              <div>
+                <div class="headline">{{article.title}}</div>
+              </div>
+            </v-card-title>
+
+            <v-card-text>{{article.content}}</v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn flat :href="article.url" target="_blank">Open</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-flex>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ app }) {
+    const { articles } = await app.$axios.$get(
+      "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=dbccf410f7cc4a22a178086ed6cffc83"
+    );
+    return { articles };
+  }
+};
+</script>
